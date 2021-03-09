46b2a1f648a4d1cd920c04c6832c62dad7423173
RudeySH <rudey@steamhunters.com>
2021-03-09 20:10:14 +0100

    Add project files.

---
+++ NTypewriterSampleClassLibrary/ClassModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTypewriterSampleClassLibrary
+{
+    public class ClassModel
+    {
+        public string Foo { get; set; }
+    }
+}
---
+++ NTypewriterSampleClassLibrary/ClassModel.ts
@@ -0,0 +1,5 @@
+﻿module App {
+    export class ClassModel {
+        public foo: string;
+    }
+}
